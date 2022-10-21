571ce5bd70b64f2097be99deb2becf3997f1f0ef
cbourgouin <charlybourgouin@gmail.com>
2022-10-21 19:05:09 +0200

    utilisation des parametre et recuperation JSON

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,16 @@
 //Mettre le code JavaScript lié à la page photographer.html
+(async () => {
+    async function getPhotographers() {
+        // Penser à remplacer par les données récupérées dans le json
+        // et bien retourner le tableau photographers seulement une fois
+        return fetch("../../data/photographers.json")
+            .then(responce => responce.json())
+            .then(json => json.photographers);
+    }
+
+    const str = document.location.href;
+    const myUrl = new URL(str);
+    const photographer = await getPhotographers();
+    const values = await photographer.filter((json) => { return json.id == myUrl.searchParams.get("photographers_id")} );
+    console.log(values);
+})();
