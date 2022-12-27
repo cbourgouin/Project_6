58d9b0c157649e2836c3da64d0aa8799033b28c3
cbourgouin <charlybourgouin@gmail.com>
2022-12-27 16:43:40 +0100

    Correction n°3 boucle modal

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -13,7 +13,7 @@ function displayModal() {
     main.setAttribute('aria-hidden', 'true');
     header.setAttribute('aria-hidden', 'true');
     floatingWindows.setAttribute('aria-hidden', 'true');
-    closeButton.focus();
+    modal.focus();
 }
 
 //utilisation du clavier pour la navigation
@@ -126,3 +126,46 @@ function verifEMail(_email) {
 
     return validation;
 }
+
+var tabbableElements = 'a[href], area[href], input:not([disabled]),' +
+	'select:not([disabled]), textarea:not([disabled]),' +
+	'button:not([disabled]), iframe, object, embed, *[tabindex],' +
+	'*[contenteditable]';
+
+var keepFocus = function (context) {
+	var allTabbableElements = context.querySelectorAll(tabbableElements);
+	var firstTabbableElement = allTabbableElements[0];
+	var lastTabbableElement = allTabbableElements[allTabbableElements.length - 1];
+
+	var keyListener = function (event) {
+		var keyCode = event.which || event.keyCode; // Get the current keycode
+
+		// Polyfill to prevent the default behavior of events
+		event.preventDefault = event.preventDefault || function () {
+			event.returnValue = false;
+		};
+
+		// If it is TAB
+		if (keyCode === 9) {
+
+			// Move focus to first element that can be tabbed if Shift isn't used
+			if (event.target === lastTabbableElement && !event.shiftKey) {
+				event.preventDefault();
+				firstTabbableElement.focus();
+
+			// Move focus to last element that can be tabbed if Shift is used
+			} else if (event.target === firstTabbableElement && event.shiftKey) {
+				event.preventDefault();
+				lastTabbableElement.focus();
+			}
+		}
+	};
+
+	context.addEventListener('keydown', keyListener, false);
+};
+
+// Call the function when the part of the page gets focus
+var modal = document.querySelector('.modal');
+keepFocus(modal);
+
+modal.focus();
